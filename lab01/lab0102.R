f8eb7b6ed0e36f6d40a4f870926fa76dce9bf80a
Alex Paulau <lacus.soli@gmail.com>
2013-10-21 10:58:35 +0300

    some improvements and sugar

--- lab01/lab0102.R
+++ lab01/lab0102.R
@@ -2,6 +2,7 @@
 library(car)
 library(MASS)
 library(knitr)
+library(psych)
 
 #' Initializing variables
 mu <- c(1, 2)
@@ -13,26 +14,34 @@ probability <- .95
 print(mu)
 print(probability)
 
+count <- 0
 for (correlation in rho) {
-  #Generating samples ~N(mu, Sigma)
-  covariance <- correlation*sigma1*sigma2
-  Sigma <- matrix(c(1, covariance, covariance, 4), 2, 2)
-  sample <- mvrnorm(n=100, mu, Sigma)
+  count <- count + 1
   print(paste("correlation: ", correlation))
   
+  # Calculating the covariance matrix
+  covariance <- correlation*sigma1*sigma2 # elements (1,2) and (2,1) in covariance matrix Sigma
+  Sigma <- matrix(c(1, covariance, covariance, 4), 2, 2)
   
+  # Generating samples ~N(mu, Sigma)
+  sample <- mvrnorm(n=100, mu, Sigma)
+    
   #Getting mean and covariance matrix for new sample
   smean <- apply(sample, 2, mean)
   scov <- cov(sample)
   
-  print("Sample mean: ")
-  print(mu)
-  print("Sigma:")
-  print(Sigma)
+  print("Sample mean:")
+  print(smean)
+  print("Sample covariance matrix:")
+  print(scov)
   
   variance <- apply(sample, 2, var)
   print("Variance: ");
   print(variance);
+  
+  # Descriptive statistics
+  dstat <- describe(sample)
+  print(dstat)
   
   # Getting the limits of the plot
   xmax <- max(sample[, 1])
@@ -40,8 +49,11 @@ for (correlation in rho) {
   xmin <- min(sample[, 1])
   ymin <- min(sample[, 2])
   
-  # Visualizing sample on plot with two elipses: expected and observed
-  plot(sample, sub="Sample", xlab="x", ylab="y", xlim=c(xmin - 1, xmax + 1), ylim=c(ymin - 1, ymax + 1), pch=21, col='blue', bg='lightblue')
+  # Visualizing sample on plot with two ellipses: expected and observed
+  plot(sample, sub=paste("Sample ", count, ", correlation=", correlation),
+       xlab="x", ylab="y",
+       xlim=c(xmin - 1, xmax + 1), ylim=c(ymin - 1, ymax + 1),
+       pch=21, col='blue', bg='lightblue')
   ellipse(smean, scov, sqrt(qchisq(probability,2)))
   ellipse(mu, Sigma, 
           sqrt(qchisq(probability, 2)), col=3, lty=2)  
